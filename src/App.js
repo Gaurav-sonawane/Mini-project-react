5b903d74a78adee2839335d34920d9cab688fa8a
GauravSonawane <gauravsonawane1368xd@gmail.com>
2022-01-26 12:14:27 +0530

    .b3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+import { useState } from "react";
+
 export default function App() {
   return (
     <div>
-      <Hello name="gaurav" />
+      <Hello />
     </div>
   );
 }
 
-//with destructuring
-function Hello({ name }) {
-  return <h1> Hello{name}</h1>;
+function Hello() {
+  let counter = 100;
+  const like = () => {
+    counter++;
+    console.log(counter);
+  };
+
+  let [counter1, setCounter1] = useState(100);
+  const like1 = () => {
+    const newCounter1 = counter1 + 1;
+    setCounter1(newCounter1);
+  };
+
+  return (
+    <div>
+      <h1>
+        {counter}
+        <button onClick={like}>Like</button>
+      </h1>
+      <h1>
+        counter1: {counter1}
+        <button onClick={like1}>Like1</button>
+      </h1>
+    </div>
+  );
 }
